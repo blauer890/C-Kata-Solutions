43e60c6f4a27f607b2630689bd4f5e347ed142a5
blauer890 <lauerb2017@gmail.com>
2025-04-17 21:13:44 -0500

    Added some template menu code

--- ZombieSurvivalv2/main.c
+++ ZombieSurvivalv2/main.c
@@ -15,5 +15,11 @@ int main(int argc, char *argv[])
     printf("\u2502 S \u250B   \u250B   \u250B   \u250B ZZ\u250B   \u2502\n");
     printf("\u2502B  \u250B   \u250B   \u250B   \u250B   \u250B   \u2502\n");
     printf("\u2517\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u251B\n");
+    printf("\n");
+    printf("Current survivor: Brian, choose an action:\n");
+    printf("Move - press 1\n");
+    printf("Melee attack - press 2\n");
+    printf("Ranged attack - press 3\n");
+    printf("Search - press 4\n");
     return 0;
 }
