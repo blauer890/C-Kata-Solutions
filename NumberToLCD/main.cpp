96765d80f29dace2f1a6f8b217ec74a593ab9bdd
blauer890 <lauerb2017@gmail.com>
2025-05-17 19:45:35 -0500

    Attempt to fix spacing issues

--- NumberToLCD/main.cpp
+++ NumberToLCD/main.cpp
@@ -19,7 +19,6 @@ int main(int argc, char *argv[])
     digitMap.insert({'8', SevenSegments(0b01111111)});
     digitMap.insert({'9', SevenSegments(0b01101111)});
 
-
     std::string number;
     std::cout << "Enter an integer: ";
     std::cin >> number;
@@ -42,49 +41,7 @@ int main(int argc, char *argv[])
     LCDStream << std::endl;
 
     std::cout << LCDStream.str();
-    /*
-    digitMap.at("0").printDigit();
-    digitMap.at("1").printDigit();
-    digitMap.at("2").printDigit();
-    digitMap.at("3").printDigit();
-    digitMap.at("4").printDigit();
-    digitMap.at("5").printDigit();
-    digitMap.at("6").printDigit();
-    digitMap.at("7").printDigit();
-    digitMap.at("8").printDigit();
-    digitMap.at("9").printDigit();
-
-    std::cout << digitMap.at("0").getTopLine() <<
-                 digitMap.at("1").getTopLine() <<
-                 digitMap.at("2").getTopLine() <<
-                 digitMap.at("3").getTopLine() <<
-                 digitMap.at("4").getTopLine() <<
-                 digitMap.at("5").getTopLine() <<
-                 digitMap.at("6").getTopLine() <<
-                 digitMap.at("7").getTopLine() <<
-                 digitMap.at("8").getTopLine() <<
-                 digitMap.at("9").getTopLine() << std::endl;
-    std::cout << digitMap.at("0").getMiddleLine() <<
-                 digitMap.at("1").getMiddleLine() <<
-                 digitMap.at("2").getMiddleLine() <<
-                 digitMap.at("3").getMiddleLine() <<
-                 digitMap.at("4").getMiddleLine() <<
-                 digitMap.at("5").getMiddleLine() <<
-                 digitMap.at("6").getMiddleLine() <<
-                 digitMap.at("7").getMiddleLine() <<
-                 digitMap.at("8").getMiddleLine() <<
-                 digitMap.at("9").getMiddleLine() << std::endl;
-    std::cout << digitMap.at("0").getBottomLine() <<
-                 digitMap.at("1").getBottomLine() <<
-                 digitMap.at("2").getBottomLine() <<
-                 digitMap.at("3").getBottomLine() <<
-                 digitMap.at("4").getBottomLine() <<
-                 digitMap.at("5").getBottomLine() <<
-                 digitMap.at("6").getBottomLine() <<
-                 digitMap.at("7").getBottomLine() <<
-                 digitMap.at("8").getBottomLine() <<
-                 digitMap.at("9").getBottomLine() << std::endl;
-    */
 
     return 0;
 }
+
